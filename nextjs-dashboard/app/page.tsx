bef59e86c1d50226de0c4788c2ae3e564e25e0b3
Wood465 <1jan.lesjak@gmail.com>
2025-11-04 10:27:45 +0100

    Začetna stran postavitev

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -2,6 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -10,22 +12,30 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+         <h1 className="text-3xl font-bold text-blue-600 md:text-4xl">
+          NutriTrack
+        </h1>
+        <p className="text-gray-700 md:text-lg leading-relaxed">
+          Aplikacija za beleženje obrokov, kalorij in spremljanje prehrane skozi dan ali teden. Uporabnik ima možnost dodajanje obrokov (ime, količina, kalorije), prikaz vnosa v grafu, ogled zgodovine, dodajanje slik, urejanje profila. Admin lahko dodaja živila v bazo, urejanje podatkov, pregled uporabnikov in njihovih dnevnikov
+        </p>
+
+                  <Link
+          href="/login"
+          className="flex items-center gap-2 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white hover:bg-blue-500 transition-colors"
+        >
+         
+          <span>Log in</span>
+        </Link>
+
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+              <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <Image
+            src="/Solata2.png"
+            alt="Healthy food and tracking app"
+            width={500}
+            height={400}
+            className="rounded-lg object-cover"
+          />
         </div>
       </div>
     </main>
